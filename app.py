f72113d1ac4d4ac09bf66e5ae69faa31555588d6
Antonio Dineros <ajmdineros@gmail.com>
2025-02-20 00:34:25 +0800

    Enabled email sending via smtplib

--- app.py
+++ app.py
@@ -7,19 +7,9 @@ import os
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
 
-# # initialize email connection
-# my_email = "ajmdineros@gmail.com"
-# password = "12345"
-# connection = smtplib.SMTP("smtp.gmail.com")
-# connection.starttls()       # makes connection secure
-#
-# connection.login(user=my_email, password=password)
-# connection.sendmail(
-#     from_addr=my_email,
-#     to_addrs="ajmdineros@gmail.com",
-#     msg="hello!"
-# )
-# connection.close()
+# initialize credentials for email
+email = os.getenv("EMAIL")
+password = os.getenv("EMAIL_PW")
 
 
 # home route
@@ -27,15 +17,44 @@ app.secret_key = os.getenv("SECRET_KEY")
 def home():
     if request.method == "POST":
         name = request.form.get("name")
-        email = request.form.get("email")
+        useremail = request.form.get("email")
         message = request.form.get("message")
-        print(name, email, message)
 
+        send_email(name, useremail, message)
         # store success message in session
         session['message_sent'] = True
 
         return redirect(url_for('home'))        # redirect to home page after submitting a message
     return render_template("index.html", message_sent=session.pop('message_sent', False))
+
+
+def send_email(name, useremail, message):
+    connection = smtplib.SMTP("smtp.gmail.com")
+    connection.starttls()  # secure connection
+
+    connection.login(user=email, password=password)
+    subject = "New Contact Form Submission"
+    body = f"""
+    You have received a new message from your portfolio contact form.
+
+    📌 Name: {name}
+    📧 Email: {useremail}
+
+    📝 Message:
+    {message}
+
+    ---
+    Best,  
+    Your Portfolio Website
+    """
+
+    msg = f"Subject: {subject}\n\n{body}"
+
+    connection.sendmail(
+        from_addr=email,
+        to_addrs="ajmdineros@gmail.com",
+        msg=msg.encode("utf-8")  # Ensures special characters are supported
+    )
 
 
 if __name__ == "__main__":
